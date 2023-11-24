241086e45e3e895fbd6356b3c131e769ba018040
Esraa Khlifah <121881132+EsraaKhlifah@users.noreply.github.com>
2023-11-24 13:26:39 +0200

    Add files via upload

---
+++ lib/screens/deatils/details_screen.dart
@@ -0,0 +1,132 @@
+import 'package:animation_2/components/fav_btn.dart';
+import 'package:animation_2/components/price.dart';
+import 'package:animation_2/constants.dart';
+import 'package:animation_2/models/Product.dart';
+import 'package:flutter/material.dart';
+
+import 'components/cart_counter.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen(
+      {Key? key, required this.product, required this.onProductAdd})
+      : super(key: key);
+
+  final Product product;
+  final VoidCallback onProductAdd;
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  String _cartTag = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: SingleChildScrollView(
+        child: SafeArea(
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: ElevatedButton(
+                onPressed: () {
+                  widget.onProductAdd();
+                  setState(() {
+                    _cartTag = '_cartTag';
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text("Add to Cart"),
+              ),
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1.37,
+              child: Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Color(0xFFF8F8F8),
+                    child: Hero(
+                      tag: widget.product.title! + _cartTag,
+                      child: Image.asset(widget.product.image!),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -20,
+                    child: CartCounter(),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: defaultPadding * 1.5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+
+                        child: Text(
+                          widget.product.title!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Price(amount: "20.00"),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(defaultPadding),
+              child: Text(
+                "Cabbage (comprising several cultivars of Brassica oleracea) is a leafy green, red (purple), or white (pale green) biennial plant grown as an annual vegetable crop for its dense-leaved heads. It is descended from the wild cabbage (B. oleracea var. oleracea), and belongs to the cole crops or brassicas, meaning it is closely related to broccoli and cauliflower (var. botrytis); Brussels sprouts (var. gemmifera); and Savoy cabbage (var. sabauda).",
+                style: TextStyle(
+                  color: Color(0xFFBDBDBD),
+                  height: 1.8,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      leading: BackButton(
+        color: Colors.black,
+      ),
+      backgroundColor: Color(0xFFF8F8F8),
+      elevation: 0,
+      centerTitle: true,
+      title: Text(
+        "Fruits",
+        style: TextStyle(color: Colors.black),
+      ),
+      actions: [
+        FavBtn(radius: 20),
+        SizedBox(width: defaultPadding),
+      ],
+    );
+  }
+}
